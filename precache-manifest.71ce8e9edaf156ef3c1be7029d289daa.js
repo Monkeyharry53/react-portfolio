6139fd428b3fb65cf578e951ae7929bc271bc084
Manjot Singh <97711975+Monkeyharry53@users.noreply.github.com>
2023-08-31 23:46:36 -0400

    Updates

---
+++ precache-manifest.71ce8e9edaf156ef3c1be7029d289daa.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "050b2d78eb5c72332d9d",
+    "url": "/react-portfolio/static/css/main.5fcc7783.chunk.css"
+  },
+  {
+    "revision": "050b2d78eb5c72332d9d",
+    "url": "/react-portfolio/static/js/main.050b2d78.chunk.js"
+  },
+  {
+    "revision": "5cc4c16838a79a03d25d",
+    "url": "/react-portfolio/static/js/1.5cc4c168.chunk.js"
+  },
+  {
+    "revision": "d80e1bcebb15672876fd",
+    "url": "/react-portfolio/static/css/2.cddd2799.chunk.css"
+  },
+  {
+    "revision": "d80e1bcebb15672876fd",
+    "url": "/react-portfolio/static/js/2.d80e1bce.chunk.js"
+  },
+  {
+    "revision": "3eca4e13473f76ee6dd3",
+    "url": "/react-portfolio/static/js/runtime~main.3eca4e13.js"
+  },
+  {
+    "revision": "74ffe8ff22244e691e0e6f989bc5743e",
+    "url": "/react-portfolio/static/media/TCW.74ffe8ff.png"
+  },
+  {
+    "revision": "5c7ed8c72c9bcd7b2c6924bd347e5084",
+    "url": "/react-portfolio/static/media/City.5c7ed8c7.jpg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/react-portfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "a0b00583d93c2f7084ad151ee0849934",
+    "url": "/react-portfolio/static/media/GithubIcon.a0b00583.svg"
+  },
+  {
+    "revision": "782b08369ca9799290ca71dff7772d6c",
+    "url": "/react-portfolio/static/media/Resume.782b0836.pdf"
+  },
+  {
+    "revision": "0342ac591c71cf8f3378e7c881e6f3e2",
+    "url": "/react-portfolio/static/media/web_development.0342ac59.svg"
+  },
+  {
+    "revision": "ff8babed00fa58dc49ce88437b77039d",
+    "url": "/react-portfolio/static/media/backend_dev.ff8babed.svg"
+  },
+  {
+    "revision": "293c9f08d398222f683d898ce43c8815",
+    "url": "/react-portfolio/static/media/mobile_dev.293c9f08.svg"
+  },
+  {
+    "revision": "8b7739a1640d0076a500922f7506723b",
+    "url": "/react-portfolio/static/media/AI.8b7739a1.svg"
+  },
+  {
+    "revision": "94998541fd01922a0a67b3ae24d7eaad",
+    "url": "/react-portfolio/static/media/Version_Collaboration.94998541.svg"
+  },
+  {
+    "revision": "89e7c3d3f1c3d1e83e88adaa56297e08",
+    "url": "/react-portfolio/static/media/Cloud_Services.89e7c3d3.svg"
+  },
+  {
+    "revision": "41e70c85e18941e890f5cd78589b6a05",
+    "url": "/react-portfolio/static/media/oopsAlgo.41e70c85.svg"
+  },
+  {
+    "revision": "b71b89f7b93ce16ac56fe3439e23b335",
+    "url": "/react-portfolio/static/media/DataAnalysisVisualization.b71b89f7.svg"
+  },
+  {
+    "revision": "3408494af81720684c329118f2b163cb",
+    "url": "/react-portfolio/static/media/TestingDebugging.3408494a.svg"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/react-portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/react-portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/react-portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "1e6831d980dfbae8557b6f1de43b46dc",
+    "url": "/react-portfolio/index.html"
+  }
+];
